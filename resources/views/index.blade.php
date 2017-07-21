290bcd4ddf09064cdd1fc7cd34a5ddeae25056fe
zStaaan <nelson_stan@hotmail.com>
2017-07-21 16:13:29 -0300

    Brasão adicionado no Shortcut icon
    Brasão da prefeitura adicionado no icone da pagina

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="shortcut icon" href="../../public/icones/brasao.png">
+	<link rel="stylesheet" type="text/css" href="">
 	<link rel="stylesheet" type="text/css" href="../../public/css/style.css">
 	<meta charset="utf-8">
 	<title>Sistema de Gestão de Documentos Técnicos e Manutenção</title>
