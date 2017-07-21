9346fd0ec9b298df3ab89e72122faf2399690082
zStaaan <nelson_stan@hotmail.com>
2017-07-21 16:03:51 -0300

    Criado template do Views principal
    Template simples do view principal criado.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="../../public/css/style.css">
+	<meta charset="utf-8">
+	<title>Sistema de Gestão de Documentos Técnicos e Manutenção</title>
+</head>
+<body>
+	
+	<div class="banner"> BANNER </div>
+	<div class="container"> CONTAINER </div>
+	<div class="footer">FOOTER</div>
+
+</body>
+</html>
